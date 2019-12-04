a4b9c0b87fcc82b1482d79a1c4c38d813674794b
Björn Åhström <ahstrom_81@hotmail.com>
2019-12-04 19:33:50 +0100

    Initial Commit

---
+++ TestingAmplify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingAmplify
+//
+//  Created by Björn Åhström on 2019-12-04.
+//  Copyright © 2019 Björn Åhström. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
